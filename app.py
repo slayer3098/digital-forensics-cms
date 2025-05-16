59cbe170eb117e3be7859ae72418f90d587a2dda
slayer3098 <usamaaltaf3098@gmail.com>
2025-05-16 17:11:28 +0500

    Update app.py and add Procfile for deployment

--- app.py
+++ app.py
@@ -218,5 +218,7 @@ def delete_case(id):
     flash('Case deleted.')
     return redirect('/cases')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
